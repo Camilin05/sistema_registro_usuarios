3bc9ec34a41fe5b93b57642f9ead40f1d1ca1f6d
Camilo_Avila <camiloam005@gmail.com>
2025-05-02 07:51:22 -0500

    Creé los archivos necesario para mi proyecto. Iniciaré a trabajar sobre esto.

---
+++ src/main/java/com/miproyecto/servlets/ConsultaServlet.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.miproyecto.servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author Personal
+ */
+@WebServlet(name = "ConsultaServlet", urlPatterns = {"/ConsultaServlet"})
+public class ConsultaServlet extends HttpServlet {
+
+}
---
+++ src/main/java/com/miproyecto/servlets/RegistroServlet.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package com.miproyecto.servlets;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+
+
+
+/**
+ *
+ * @author Personal
+ */
+@WebServlet(name = "RegistroServlet", urlPatterns = {"/RegistroServlet"})
+public class RegistroServlet extends HttpServlet {
+
+}
